0f39579b7615bba9d630de1660f146c95e0f7727
Buthukuri Venkata Srinivasa Reddy <b.b.srinivasa.reddy@accenture.com>
2022-06-23 16:56:01 +0000

    update changes

--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -4,15 +4,17 @@ service MroService @(impl : './cat-service.js') {
     @readonly
     entity Roles             as projection on my.Roles;
 
-    @readonly
     entity Users             as projection on my.Users_Role_Assign;
 
     @readonly
     entity MaintainApproval  as projection on my.User_Approve_Maintain;
+
 
     @readonly
     entity VendorList        as projection on my.Vendor_List;
 
     @readonly
     entity PricingConditions as projection on my.Pricing_Conditions;
+
+    entity UpdateUsers       as projection on my.Users_Role_Assign;
 }
--- srv/cat-service.js
+++ srv/cat-service.js
@@ -8,14 +8,40 @@ module.exports = async function () {
     this.on("READ", "Roles", async (req, res) => {
         // const { SELECT } = cds.ql(req);
         var aFilter = req.query.SELECT.where;
-        if(aFilter === undefined){
+        if (aFilter === undefined) {
             return await SELECT.from(Roles);
         }
         const result = await SELECT.from(Roles).where(aFilter);
         // const result = await SELECT.from(Users_Role_Assign);
-        const result_where = await SELECT.from(Users_Role_Assign).where({role:"CDT"});
+        const result_where = await SELECT.from(Users_Role_Assign).where({ role: "CDT" });
         console.log(result);
         return result;
         // return result;
+    });
+
+    this.on("UPDATE", "UpdateUsers", async (req, next) => {
+        const user = req.data.user;
+        const role = req.data.role;
+        var result = await next();
+        const db = await cds.connect.to('db')
+        const {
+            Roles,
+            Users_Role_Assign
+        } = db.entities("ferrero.mro");
+        const aRoles = await SELECT.from(Roles).where({ role: req.data.role });
+        if (aRoles.length > 0) {
+            try {
+                let result1 = await UPDATE(Users_Role_Assign)
+                    .set({
+                        role: req.data.role
+                    }).where({ user: req.data.user });
+                console.log(result);
+            } catch (err) {
+                return err;
+            }
+            return result;
+        }else{
+            req.error(400, "Invalid Role " +  req.data.role);
+        }
     });
 }
